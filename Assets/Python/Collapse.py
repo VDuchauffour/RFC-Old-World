e248bc1ca8344c7856b6c8c6a45af6c1495fbeeb
dguenms <dguenms@gmail.com>
2025-03-18 00:08:22 +0100

    Remove redundant code

--- Assets/Python/Collapse.py
+++ Assets/Python/Collapse.py
@@ -94,11 +94,6 @@ def downgradeImprovements(iPlayer):
 		else:
 			plot.setImprovementType(-1)
 		
-		# Destroy all Harappan and Toltec improvements
-		if civ(iPlayer) in [iHarappa, iToltecs] and not player(iPlayer).isHuman():
-			if iImprovement >= 0:
-				plot.setImprovementType(-1)
-				
 	message(iPlayer, 'TXT_KEY_STABILITY_DOWNGRADE_IMPROVEMENTS', color=iRed)
 		
 def collapseToCore(iPlayer):
