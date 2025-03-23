a5cb8a73e1c9918e88377c8d1e06350652d3d4cd
dguenms <dguenms@gmail.com>
2025-03-23 22:42:51 +0100

    Arabia starts with sea units

--- Assets/Python/History.py
+++ Assets/Python/History.py
@@ -511,6 +511,13 @@ def createArabArmies(iPlayer):
 
 		if not player(iArabia).isHuman() and bBaghdad:
 			makeUnits(iArabia, iSpearman, tBaghdad, 2)
+		
+		coastal_city = cities.owner(iArabia).coastal().random()
+		if coastal_city:
+			createRoleUnit(iArabia, coastal_city, iEscort, 2)
+			createRoleUnit(iArabia, coastal_city, iExploreSea, 1)
+			createRoleUnit(iArabia, coastal_city, iSettleSea, 1)
+			createRoleUnit(iArabia, coastal_city, iFerry, 1)
 	
 
 @handler("flip")
